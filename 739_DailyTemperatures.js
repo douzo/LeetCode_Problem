a6e492c4a2cccb0816048413f0f51b4f40bdfab5
douzo <kadhiresananand@gmail.com>
2024-03-28 23:54:49 +0900

    739 Daily Temperatures

---
+++ 739_DailyTemperatures.js
@@ -0,0 +1,37 @@
+/**
+ * 739. Daily Temperatures
+ * 
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+ */
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (temperatures) {
+  var result = new Array(temperatures.length).fill(0)
+  var stack = new Array()
+
+  for (let i = 0; i < temperatures.length; i++) {
+      while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+          let stackIndex = stack.pop()
+          result[stackIndex] = i - stackIndex
+      }
+      stack.push(i)
+  }
+  return result
+};
